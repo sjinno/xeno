239603182e4f211b748781fb0cc5fc21faad84df
Shohei Jinno <shohei.jinno@gmail.com>
2022-02-06 10:05:36 -0800

    Update GameSteps trait.

--- src/main.rs
+++ src/main.rs
@@ -127,8 +127,11 @@ struct Game {
 
 trait GameSteps {
     fn new() -> Game;
-    fn direction();
+    fn setup();
+    fn chose_direction();
     fn choose_initial_player();
+    fn begin();
+    fn restart();
 }
 
 // impl GameSteps for Game {
@@ -137,6 +140,10 @@ trait GameSteps {
 // }
 
 // [x] 0. Create a deck of 18 cards
+// Oh, come to think of it, probably I don't even need to create a deck
+// I'll just have to keep track of which card was drawn in a map to make sure
+// that each draw, decrement the count total.
+
 // [x] 1. Create cards Rank 1-8 x 2, Rank 9, 10 x 1 --- totaling 18 cards
 // [x] 2. Shuffle cards
 // [ ] 3. Coose clockwise or counterclockwise (default clockwise)
