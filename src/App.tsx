7552df15aa31e9aff1aadb9a1fc33d2c243fbd99
Shohei Jinno <shohei.jinno@gmail.com>
2025-01-11 09:20:54 -0800

    feat: set up shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  return <main className="container">main</main>;
+  return (
+    <main className="container">
+      <Button>Click me</Button>
+    </main>
+  );
 }
 
 export default App;
