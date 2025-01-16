421ec5a3c761aabcedf2ff1b04081168ea6bd272
Shohei Jinno <shohei.jinno@gmail.com>
2025-01-16 09:46:35 -0800

    fix: rename GAME_PATH to VITE_GAMES_COLLECTION

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const GAME_PATH = import.meta.env.VITE_GAME_PATH;
+export const GAME_PATH = import.meta.env.VITE_GAMES_COLLECTION;
 export const GAME_PUBLIC_ID = import.meta.env.VITE_GAME_PUBLIC_ID;
 export const GAME_PRIVATE_ID = import.meta.env.VITE_GAME_PRIVATE_ID;
